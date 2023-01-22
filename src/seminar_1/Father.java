d051c88321dfa2aafad6018719bcdd1249b52a96
MrSsmaill <180596@mail.ru>
2023-01-22 22:46:55 +0300

    dz1

---
+++ src/seminar_1/Father.java
@@ -0,0 +1,10 @@
+package seminar_1;
+
+
+public class Father extends Human {
+
+
+    public Father(String name, String gender, boolean liveStatus, Human mother, Human father, Human next) {
+        super(name, gender, liveStatus, mother, father, next);
+    }
+}
---
+++ src/seminar_1/Mother.java
@@ -0,0 +1,9 @@
+package seminar_1;
+
+import java.util.ArrayList;
+
+public class Mother extends Human{
+    public Mother(String name, String gender, boolean liveStatus, Human mother, Human father, Human next) {
+        super(name, gender, liveStatus, mother, father, next);
+    }
+}
